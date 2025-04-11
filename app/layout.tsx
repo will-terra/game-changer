8cff98858d0d19296e4ca7e33e56f2c8ca9913bd
will-terra <wpterra@outlook.com>
2025-04-11 13:40:34 -0300

    Feat: Project setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
 import type { ReactNode } from "react";
 import { StoreProvider } from "./StoreProvider";
-import { Nav } from "./components/Nav";
 
 import "./styles/globals.css";
-import styles from "./styles/layout.module.css";
 
 interface Props {
   readonly children: ReactNode;
@@ -13,71 +10,20 @@ interface Props {
 export default function RootLayout({ children }: Props) {
   return (
     <StoreProvider>
-      <html lang="en">
-        <body>
-          <section className={styles.container}>
-            <Nav />
-
-            <header className={styles.header}>
-              <Image
-                src="/logo.svg"
-                className={styles.logo}
-                alt="logo"
-                width={100}
-                height={100}
-              />
-            </header>
-
-            <main className={styles.main}>{children}</main>
-
-            <footer className={styles.footer}>
-              <span>Learn </span>
-              <a
-                className={styles.link}
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://redux.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://redux-toolkit.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux Toolkit
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://react-redux.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React Redux
-              </a>
-              ,<span> and </span>
-              <a
-                className={styles.link}
-                href="https://reselect.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Reselect
-              </a>
-            </footer>
-          </section>
+      <html lang="en" className="min-h-screen p-0 m-0 font-sans">
+        <body className="min-h-screen p-0 m-0 flex flex-col font-sans">
+          <header ></header>
+          <main className="flex grow flex-col items-center justify-center">{children}</main>
+          <footer className="justify-center self-center">
+            <span>Learn </span>
+            <a
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React
+            </a>
+          </footer>
         </body>
       </html>
     </StoreProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Counter } from "./components/counter/Counter";
 
 export default function IndexPage() {
-  return <Counter />;
+  return <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>;
 }
 
 export const metadata: Metadata = {
-  title: "Redux Toolkit",
+  title: "Game Changer",
 };
