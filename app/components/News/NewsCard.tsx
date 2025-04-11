968123a2a96ec6740baadcb458f3576f29e0cba0
will-terra <wpterra@outlook.com>
2025-04-11 18:28:20 -0300

    Feat: news product card

---
+++ app/components/News/NewsCard.tsx
@@ -0,0 +1,23 @@
+import { Product } from "@/lib/features/services/productsAPI";
+import Image from "next/image";
+
+interface NewsCardProps extends Product {}
+
+export default function NewsCard({
+  name,
+  images,
+  price,
+  description,
+}: NewsCardProps) {
+  return (
+    <div className="bg-primary-midBlue border-primary-lightBlue/55 flex w-60 flex-col items-center justify-center border pb-3">
+      <div className="flex grow items-center">
+        <Image src={images[0]} alt={name} width={160} height={88} />
+      </div>
+      <h2 className="mx-1 text-white">{name}</h2>
+      <p className="bg-secondary-midBlue px-4 py-1 text-white">
+        {price} Quantions
+      </p>
+    </div>
+  );
+}
