448fa5df324286a50bc775cdcb27d9b44d365021
will-terra <wpterra@outlook.com>
2025-04-14 15:32:12 -0300

    Feat: CTA Banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { CTABanner } from "@/lib/features/products/components/CTABanner";
 import { HighlightedItems } from "@/lib/features/products/components/HighlightedItems";
 import { ItemsDisplay } from "@/lib/features/products/components/ItemsDisplay";
 import { News } from "@/lib/features/products/components/News";
@@ -9,6 +10,12 @@ export default function IndexPage() {
       <News />
       <ItemsDisplay />
       <HighlightedItems />
+      <CTABanner
+        futureDate={new Date(2025, 3, 27, 12, 0, 0)}
+        title="Five M Nite"
+        subTitle="50% de desconto"
+        description="Lorem ipsum dolor sit amet consectetur. Aliquet ac massa metus faucibus viverra laoreet varius. Sit ut."
+      />
     </div>
   );
 }
---
+++ lib/features/products/components/CTABanner.tsx
@@ -0,0 +1,63 @@
+"use client";
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+import Image from "next/image";
+
+interface CTABannerProps {
+  title: string;
+  subTitle: string;
+  description: string;
+  futureDate: Date;
+}
+
+export function CTABanner(props: CTABannerProps) {
+  const { title, subTitle, description, futureDate } = props;
+
+  return (
+    <div className="bg-primary-midBlue flex h-[48rem] w-screen items-center">
+      <div className="ml-32 flex">
+        <div className="flex h-full max-w-[27.25rem] flex-col items-start justify-center gap-8">
+          <h2 className="text-[40px] font-bold text-white uppercase">
+            {title}
+          </h2>
+          <p className="bg-secondary-blue px-12 py-4 text-xl font-bold text-white uppercase">
+            {subTitle}{" "}
+          </p>
+
+          <p className="text-white">{description}</p>
+          {futureDate && (
+            <FlipClockCountdown
+              to={futureDate}
+              labels={["DAYS", "HOURS", "MINUTES", "SECONDS"]}
+              showSeparators={false}
+              labelStyle={{
+                fontSize: 10,
+                fontWeight: 500,
+                textTransform: "uppercase",
+                color: "#fff",
+              }}
+              digitBlockStyle={{ width: 40, height: 60, fontSize: 30 }}
+              duration={0.5}
+            />
+          )}
+
+          <Image
+            src="/CTAButton.svg"
+            alt="Acessar Jogo"
+            width={232}
+            height={48}
+            className="mt-2 hover:fill-amber-300"
+          />
+        </div>
+      </div>
+      <div className="relative ml-auto h-full w-3/5">
+        <Image
+          src="/CTABanner.png"
+          alt="Five M Nite"
+          fill
+          className="absolute right-0 max-w-[66rem]"
+        />
+      </div>
+    </div>
+  );
+}
