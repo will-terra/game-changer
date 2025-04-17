6eb305ca541d9d6df7a5ddbc2ef09fd399c29762
will-terra <wpterra@outlook.com>
2025-04-17 10:14:41 -0300

    Style: Blue Button

--- lib/features/products/components/BlueButton.tsx
+++ lib/features/products/components/BlueButton.tsx
@@ -10,7 +10,7 @@ export function BlueButton({ href, children }: BlueButtonProps) {
     <Link href={href} className="group mt-2 mb-12 inline-block">
       <div className="relative h-12 w-[14.5rem] group-hover:shadow-[8px_8px_16px_rgba(73,150,213,0.7)]">
         <div className="border-secondary-blue group-hover:border-secondary-blue/70 absolute inset-0 border-[1.125px]"></div>
-        <div className="group-hover:bg-secondary-blue/70 bg-secondary-blue absolute top-[10%] right-[2%] bottom-[8.33%] left-[2%]"></div>
+        <div className="group-hover:bg-secondary-blue/70 bg-secondary-blue absolute top-[6%] right-[2%] bottom-[8.33%] left-[2%]"></div>
         <div className="absolute top-[7px] left-[18px] mt-1 h-[27px] w-[11.625rem] text-center text-[18px] leading-[27px] font-bold text-white group-hover:text-right">
           {children}
         </div>
